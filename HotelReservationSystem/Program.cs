0cd09afb4504607b4bcda22d1ecdc4c24a01ccfc
venkey <venkeyvenkatesh885@gmail.com>
2020-11-04 11:05:00 +0530

    first commit

---
+++ HotelReservationSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HotelReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to  Hotel Reservation System");
+
+          
+           
+
+        }
+    }
+}
